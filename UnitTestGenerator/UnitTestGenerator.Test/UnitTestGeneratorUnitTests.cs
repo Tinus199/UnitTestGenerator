55dbd9d3528613696641fcffdac618972419e7aa
Tinus199 <tinusbotha9@gmail.com>
2024-08-12 22:40:07 +0200

    Small updates

--- UnitTestGenerator/UnitTestGenerator.Test/UnitTestGeneratorUnitTests.cs
+++ UnitTestGenerator/UnitTestGenerator.Test/UnitTestGeneratorUnitTests.cs
@@ -9,7 +9,6 @@ namespace UnitTestGenerator.Test
     [TestClass]
     public class UnitTestGeneratorUnitTest
     {
-        //No diagnostics expected to show up
         [TestMethod]
         public async Task TestMethod1()
         {
@@ -18,42 +17,10 @@ namespace UnitTestGenerator.Test
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
 
-        //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
         public async Task TestMethod2()
         {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
 
-    namespace ConsoleApplication1
-    {
-        class {|#0:TypeName|}
-        {   
-        }
-    }";
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-
-            var expected = VerifyCS.Diagnostic("UnitTestGenerator").WithLocation(0).WithArguments("TypeName");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
         }
     }
 }
